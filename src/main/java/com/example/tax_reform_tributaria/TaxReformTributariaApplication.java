8fb0273a797c7669f4bcfeb0f7fdeda05ba4f480
Matheus Faias <faiaxx@MacBook-Air-de-Matheus.local>
2026-03-25 18:04:47 -0300

    feat: primeiro commit

---
+++ src/main/java/com/example/tax_reform_tributaria/TaxReformTributariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tax_reform_tributaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxReformTributariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxReformTributariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tax_reform_tributaria/TaxReformTributariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tax_reform_tributaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxReformTributariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
